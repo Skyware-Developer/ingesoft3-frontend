16ef0eeccd98cabf62643afd9141e13677ab6395
Gabriel Bateca <gabriel.bateca@outlook.com>
2022-02-27 16:44:56 -0500

    Desarrollo de plantilla principal del sitio web

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
