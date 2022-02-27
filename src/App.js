f3520761221c78d0cc03997f77196c1e9244c69d
Gabriel Bateca <gabriel.bateca@outlook.com>
2022-02-27 17:39:46 -0500

    Configuracion de firebase y setup de firebase auth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+
 function App() {
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.render(
   
